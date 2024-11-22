148c5ac704cb729cd0f9df3d1e744a570a52f8a8
Ritik6559 <ritikjoshi741@gmail.com>
2024-11-22 23:41:51 +0530

    adding home screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import { Asset } from 'expo-asset';
+import * as React from 'react';
+import { SQLiteProvider } from 'expo-sqlite'
+import Home from './screens/Home';
+import { createStaticNavigation, NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+const loadDatabase = async () => {
+  const dbName = "mySQLiteDB.db";
+
+  const dbAsset = require("./assets/mySQLiteDB.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [dbLoaded, setDbLoaded] = React.useState<boolean>(false);
+
+
+  React.useEffect(() => {
+    loadDatabase()
+      .then(() => setDbLoaded(true))
+      .catch((e) => console.error(e))
+  }, [])
+
+  if (!dbLoaded) {
+    <View style={{ flex: 1 }}>
+      <ActivityIndicator size={"large"} />
+      <Text>
+        Loading Expenses....
+      </Text>
     </View>
+  }
+
+  return (
+    <NavigationContainer>
+      <React.Suspense
+        fallback={
+          <View style={{ flex: 1}}>
+            <ActivityIndicator size={"large"} />
+            <Text>
+              Loading Expenses....
+            </Text>
+          </View>
+        }
+      >
+        <SQLiteProvider
+          databaseName='mySQLiteDB.db'
+          useSuspense
+        >
+          <Stack.Navigator >
+            <Stack.Screen name="Home" component={Home} options={{
+              headerTitle: "Track It",
+              headerLargeTitle: true,
+              headerShadowVisible: false,
+
+            }} />
+          </Stack.Navigator>
+        </SQLiteProvider>
+      </React.Suspense>
+    </NavigationContainer>
+
   );
 }
 
